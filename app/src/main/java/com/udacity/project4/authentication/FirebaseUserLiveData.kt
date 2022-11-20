f542a00b5381901bd429513d39459c7afc0f67fa
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-11-20 18:20:50 +0500

    Reviewing existing codebase

--- app/src/main/java/com/udacity/project4/authentication/FirebaseUserLiveData.kt
+++ app/src/main/java/com/udacity/project4/authentication/FirebaseUserLiveData.kt
@@ -7,8 +7,20 @@ import com.google.firebase.auth.FirebaseUser
 /**
  * This class observes the current FirebaseUser. If there is no logged in user, FirebaseUser will
  * be null.
+ *
+ * Note that onActive() and onInactive() will get triggered when the configuration changes (for
+ * example when the device is rotated). This may be undesirable or expensive depending on the
+ * nature of your LiveData object, but is okay for this purpose since we are only adding and
+ * removing the authStateListener.
  */
 class FirebaseUserLiveData : LiveData<FirebaseUser?>() {
+
+    // I am obtaining an instance of the FirebaseAuth class by calling getInstance() method.
+    //
+    // Theory:
+    // Firebase Auth enables you to subscribe in realtime to this state via a Stream . Once called,
+    // the stream provides an immediate event of the user's current authentication state, and then
+    // provides subsequent events whenever the authentication state changes.
     private val firebaseAuth = FirebaseAuth.getInstance()
 
     // I am setting the value of this FireUserLiveData object by hooking it up to equal the value of
@@ -33,4 +45,5 @@ class FirebaseUserLiveData : LiveData<FirebaseUser?>() {
     override fun onInactive() {
         firebaseAuth.removeAuthStateListener(authStateListener)
     }
+
 }
