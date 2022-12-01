1ea0679b6f29b43eaeee6f4f8c89d3426ab30f01
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-01 12:09:42 +0500

    Reviewing & Adding comments

--- app/src/main/java/com/udacity/project4/base/DataBindingViewHolder.kt
+++ app/src/main/java/com/udacity/project4/base/DataBindingViewHolder.kt
@@ -11,8 +11,28 @@ class DataBindingViewHolder<T>(private val binding: ViewDataBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: T) {
+
+
+        /**
+         * Set the variable id of the data item to the binding
+         *
+         * EXPLANATION
+         * ===========
+         * 1. At times, the specific binding class isn't known. For example, a RecyclerView.Adapter
+         * operating against arbitrary layouts doesn't know the specific binding class. It still must
+         * assign the binding value during the call to the onBindViewHolder() method.
+         *
+         * 2. The Data Binding Library generates a class named BR in the module package which
+         * contains the IDs of the resources used for data binding. In the following code, the
+         * library automatically generates the BR.item variable.
+         *
+         * Reference: https://developer.android.com/topic/libraries/data-binding/generated-binding
+         */
         binding.setVariable(BR.item, item)
+
+        // This is necessary to ensure the data binding is executed immediately
         binding.executePendingBindings()
+
     }
 
 }
