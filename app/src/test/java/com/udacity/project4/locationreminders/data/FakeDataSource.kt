fbd9f3e7e45223b30c7225de7292fab0e2a8fc62
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-23 23:12:32 +0500

    + Comments: FakeDataSource.kt

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -3,62 +3,62 @@ package com.udacity.project4.locationreminders.data
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
+// FakeDataSource will act as a test double for the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
-    // DONE: Create a fake data source to act as a double to the real data source
+    // reminderServiceData will be used to save reminders in the fake data source
     private var remindersServiceData: LinkedHashMap<String, ReminderDTO> = LinkedHashMap()
 
+    // Default false value for the shouldReturnError variable
     private var shouldReturnError = false
 
+    // This function will update the value for the shouldReturnError variable
     fun setReturnError(value: Boolean) {
         shouldReturnError = value
     }
 
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-
-        // ERROR
-        // shouldReturnError is a boolean flag that can be set to true to force the
-        // getReminders() method to return an error.
-        if (shouldReturnError) {
-            return Result.Error("Test exception")
-        }
-
-        // Return the reminders
-        return Result.Success(remindersServiceData.values.toList())
-
-    }
-
-    override suspend fun saveReminder(reminder: ReminderDTO) {
-
-        // Add reminder to the remindersServiceData
-        remindersServiceData.values.add(reminder)
-
-    }
-
-    // getRemainder() returns a Result object. If the Result object is a success, then
-    // the reminder is returned. If the Result object is an error, then the error is
-    // returned.
+    // getReminder() = This function will return a reminder from the fake data source
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
 
-        // shouldReturnError is a boolean flag that can be set to true to force the
-        // getReminders() method to return an error.
+        // If an error has been found while retrieving the reminders, then return an error message
         if (shouldReturnError) {
             return Result.Error("Test exception ")
         }
 
-        // SUCCESS - Check if the reminder is in the list
+        // SUCCESS - Upon success, return the outcome message
         remindersServiceData[id]?.let {
             return Result.Success(it)
         }
 
-        // ERROR - Return an error
+        // ERROR - Return an error message: "Could not find reminder"
         return Result.Error("Could not find reminder")
     }
 
+    // getReminders() = This function will return the list of reminders from the fake data source
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+
+        // If an error has been found while retrieving the reminders, then return an error message
+        if (shouldReturnError) {
+            return Result.Error("Test exception")
+        }
+
+        // Otherwise, return the list of reminders
+        return Result.Success(remindersServiceData.values.toList())
+
+    }
+
+    // saveReminder() = This function will save a reminder in the fake data source
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+
+        // Add reminder to the remindersServiceData (fake data source)
+        remindersServiceData.values.add(reminder)
+
+    }
+
+    // deleteAllReminders() = This function will delete all reminders from the fake data source
     override suspend fun deleteAllReminders() {
 
-        // Clear the data to avoid test pollution.
+        // Clear the data
         remindersServiceData.clear()
 
     }
