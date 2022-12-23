bbf6eb661ec01c526ffbb8d0f2ae03ca5fe8cac4
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-24 02:40:11 +0500

    + Comments: TestUtil.kt

--- app/src/test/java/com/udacity/project4/locationreminders/TestUtil.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/TestUtil.kt
@@ -7,7 +7,6 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
-
 @VisibleForTesting(otherwise = VisibleForTesting.NONE)
 fun <T> LiveData<T>.getOrAwaitValue(
     time: Long = 2,
@@ -15,31 +14,55 @@ fun <T> LiveData<T>.getOrAwaitValue(
     afterObserve: () -> Unit = {}
 ): T {
 
+    // data is initially set to null
     var data: T? = null
 
+    // latch is used to block the thread until the data is set
     val latch = CountDownLatch(1)
 
+    // This will be used to observe the live data
     val observer = object : Observer<T> {
+
+        // on changed is called when the data is set
         override fun onChanged(o: T?) {
+
+            // set the data
             data = o
+
+            // unblock the thread
             latch.countDown()
+
+            // remove the observer
             this@getOrAwaitValue.removeObserver(this)
+
         }
+
     }
 
+    // Observe the LiveData forever
     this.observeForever(observer)
 
     try {
+
+        // afterObserve is called to perform any action before blocking the thread
         afterObserve.invoke()
 
+        // block the thread until the data is set or the timeout is reached
         if (!latch.await(time, timeUnit)) {
+
+            // if the timeout is reached throw an exception error: "LiveData value was never set."
             throw TimeoutException("LiveData value was never set.")
+
         }
 
     } finally {
+
+        // removeObserver is called to remove the observer
         this.removeObserver(observer)
+
     }
 
+    // return the data
     @Suppress("UNCHECKED_CAST")
     return data as T
 }
