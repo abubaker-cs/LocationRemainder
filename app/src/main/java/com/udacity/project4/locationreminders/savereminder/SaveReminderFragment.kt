312f985a77501156bcb1204c6069b6a38054012a
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-24 14:39:11 +0500

    + Comments: RemindersActivity.kt

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -33,13 +33,12 @@ import com.udacity.project4.databinding.FragmentSaveReminderBinding
 import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
-import org.koin.android.ext.android.inject
 import java.util.*
 
 class SaveReminderFragment : BaseFragment() {
 
     //Get the view model this time as a single to be shared with the another fragment
-    override val _viewModel: SaveReminderViewModel by inject() // sharedViewModel
+    override val _viewModel: SaveReminderViewModel by sharedViewModel() // sharedViewModel
 
     private var _binding: FragmentSaveReminderBinding? = null
     private val binding get() = _binding!!
