a51ddfde8876560a93229363b2f44d662c02201c
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-12-24 14:40:14 +0500

    + Comments: RemindersActivity.kt

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -33,6 +33,7 @@ import com.udacity.project4.databinding.FragmentSaveReminderBinding
 import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.util.*
 
 class SaveReminderFragment : BaseFragment() {
